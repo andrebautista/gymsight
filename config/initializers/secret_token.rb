a9ca74aa254d657cbe65dc0be38f3e76ad4de73a
Andre Bautista <andrebautista2513@gmail.com>
2014-01-06 15:09:25 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gymsight::Application.config.secret_key_base = '7b4478f6af3f6088cdd26548972467ecf1b7fee3d3e09717f4ff77c71989cf5627850faafe29d2e8e4761452735622077be10ed3299f0a24134e3ce12a2c2e3f'
