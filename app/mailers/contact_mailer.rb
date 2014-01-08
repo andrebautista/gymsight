e83cbcd10e6a18ee388e74d2958224bae7dacc65
Andre Bautista <andrebautista2513@gmail.com>
2014-01-07 17:49:20 -0800

    added mailer and postmark

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,15 @@
+class ContactMailer < ActionMailer::Base
+  default from: "contact@gymsight.com"
+  default to: "dewayne@gymsight.com"
+
+  def consultation_request
+
+
+
+  end
+
+  def question
+
+
+  end
+end
