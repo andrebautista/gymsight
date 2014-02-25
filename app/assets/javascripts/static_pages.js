4b544a72b2de72e1ca62660c0caf77dac4519ed9
Andre Bautista <andrebautista2513@gmail.com>
2014-02-24 21:08:47 -0800

    got JS event working on homepage

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -9,13 +9,14 @@ $(".nav-tile").hover(
     function() {
      var tileHeight = $(".nav-tile").height();
     $(this).children(".nav-details").animate({
-      height: tileHeight + 1  }, 200
+      height: tileHeight + 1  }, 300
       );
   }, function() {
     var captionHeight = $(".image-title").height();
-    $(this).children("nav-details").animate({
-      height: captionHeight }, 200
+    $(this).children(".nav-details").animate({
+      height: captionHeight }, 300
     );
   }
 );
+
 
