70f97861750dafbf5cae4f46cde94b75c43fe6c5
Andre Bautista <andrebautista2513@gmail.com>
2014-02-24 16:15:56 -0800

    working on hover leave event

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,21 @@
+$('#slideShow').ayaSlider({
+     easeIn : 'easeOutBack',
+     easeOut : 'linear',
+     delay : 4000
+  });
+
+
+$(".nav-tile").hover(
+    function() {
+     var tileHeight = $(".nav-tile").height();
+    $(this).children(".nav-details").animate({
+      height: tileHeight + 1  }, 200
+      );
+  }, function() {
+    var captionHeight = $(".image-title").height();
+    $(this).children("nav-details").animate({
+      height: captionHeight }, 200
+    );
+  }
+);
+
