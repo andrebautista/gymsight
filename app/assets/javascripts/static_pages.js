1228e72e530a6996f032690694c9900107de96f7
Andre Bautista <andrebautista2513@gmail.com>
2014-03-12 13:56:07 -0700

    fixed sized images" "

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -5,25 +5,25 @@ $('#slideShow').ayaSlider({
   });
 
 
-$(".nav-tile").hover(
-    function() {
-     var tileHeight = $(".nav-tile").height();
-    $(this).children(".nav-details").animate({
-      height: tileHeight + 1  }, 300
-      );
-  }, function() {
-    var captionHeight = $(".image-title").height();
-    $(this).children(".nav-details").animate({
-      height: captionHeight }, 300
-    );
-  }
-);
+// $(".nav-tile").hover(
+//     function() {
+//      var tileHeight = $(".nav-tile").height();
+//     $(this).children(".nav-details").animate({
+//       height: tileHeight + 1  }, 300
+//       );
+//   }, function() {
+//     var captionHeight = $(".image-title").height();
+//     $(this).children(".nav-details").animate({
+//       height: captionHeight }, 300
+//     );
+//   }
+// );
 
-$(window).resize(function() {
-  if ($(window).width() < 652)  {
-    var picHeight = $(".banner-image").height();
-    $("#slideShow").css("height", picHeight);
-  }
-});
+// $(window).resize(function() {
+//   if ($(window).width() < 652)  {
+//     var picHeight = $(".banner-image").height();
+//     $("#slideShow").css("height", picHeight);
+//   }
+// });
 
 
