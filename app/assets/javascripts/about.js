f70369d43b6ae745a9e65ed0b08a268b68227f0d
Andre Bautista <andrebautista2513@gmail.com>
2014-04-03 14:55:18 -0700

    removed auto height of content box, dealing with unequal elements, moving on

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,6 +1,18 @@
-//trainer-bio prime
+// $('.bio-page').on('click', function() {
+//   var $headshot = $("#gymowner-headshot");
+//         console.log("loaded");
+//         var picHeight = $headshot.height().delay(500);
+//         console.log(picHeight);
+//         $("#gymowner-bio").css("height", picHeight);
+// });
 
-$(window).resize(function() {
-    var picHeight = $("#gymowner-headshot").height();
-    $("#gymowner-bio").css("height", picHeight);
-});
+// $(window).resize(function() {
+//     var $headshot = $("#gymowner-headshot");
+//     $headshot.ready(function(){
+//         var picHeight = $headshot.height();
+//         $("#gymowner-bio").css("height", picHeight);
+//     });
+// });
+
+
+
