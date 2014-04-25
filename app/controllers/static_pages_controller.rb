86531395954d637572edde9d285d77984700e520
Andre Bautista <andrebautista2513@gmail.com>
2014-04-25 15:12:56 -0700

    working on programs layout page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,9 +9,14 @@ class StaticPagesController < ApplicationController
   end
 
   def offerings
+    @link_class = "hidden"
   end
 
   def gap
+    @banner_text_l1 = "General"
+    @banner_text_l2 = "Athletic Program"
+    @banner_image = "what-we-offer-banner"
+    @link_class = "back-to-offerings"
   end
 
   def gallery
