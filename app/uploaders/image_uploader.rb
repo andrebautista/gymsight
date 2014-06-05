5a78700d64e9919de4bbd215d944400e9c2e81ef
Andre Bautista <andrebautista2513@gmail.com>
2014-06-05 16:55:08 -0700

    changed directory to proper one for heroku

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,9 +17,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.id}"
   end
 
+  # for local: "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   # def cache_dir
   #   "#{Rails.root}/tmp/uploads"
   # end
