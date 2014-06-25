2c799b32ba95f2b1d09c5920ef12cb83f6d0a50a
Andre Bautista <andrebautista2513@gmail.com>
2014-06-25 11:48:38 -0700

    configuring cloudfront for gallery

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id     => ENV['S3_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET'],
   }
+  config.asset_host = "http://d1glu0vbbg0go6.cloudfront.net"
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
   config.fog_directory    = ENV['S3_BUCKET_NAME']
   config.fog_public       = true
