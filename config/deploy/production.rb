99fc605369d11ea11ea6f43d1822a92496600b71
Andre Bautista <andrebautista2513@gmail.com>
2014-06-30 00:04:23 -0700

    trying figaro setup for server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,8 +5,8 @@ set :stage, :production
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-# role :app, %w{deploy@example.com}
-# role :web, %w{deploy@example.com}
+role :app, %w{deploy@gymsight.com}
+role :web, %w{deploy@gymsight.com}
 # role :db,  %w{deploy@example.com}
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ set :stage, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '198.199.103.189', user: 'deploy'
+server '198.199.103.189', user: 'deploy', roles: %w{web app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
